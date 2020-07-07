358c3b64106396c8afaf111c4e4939f3700a5acd
gglatilla <guido@nusaf.co.za>
2020-07-07 15:33:13 +0300

    tag added but not working

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
+
 /*** 
  * `quotes` array 
 ***/
@@ -16,30 +17,35 @@ let quotes = [
     source: 'Niki Lauda',
     citation: 'Rush',
     year: '2013',
+    tag: 'Educational',
   },
   {
     quote: 'Every man dies, but not everyman really lives.',
     source: 'William Wallace',
     citation: 'Braveheart',
     year: '1995',
+    tag: 'Inspirational',
   },
   {
-    quote: "Great men are not born great =, they grow great.",
+    quote: "Great men are not born great, they grow great.",
     source: 'Mario Puzo',
     citation: 'The Godfather',
     year: '1972',
+    tag: 'Inspirational',
   },
   {
     quote:"Nobody is gonna hit as hard as life, but it ain't how hard you can hit. It's how hard you can get hit and keep moving forward. It's how much you can take, and keep moving forward. That's how winning is done.",
     source:'Rocky Balboa',
     citation: 'Rocky Balboa',
     year: '2006',
+    tag: 'Educational',
   },
   {
     quote: "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure. It is our light, not our darkness, that most frightens us. We ask ourselves, who am I to be brilliant, gorgeous, talented and fabulous? Actually, who are you not to be? You are a child of God. Your playing small doesn't serve the world. We were born to make manifest the glory of God that is within us. It's not just in some of us; it's in everyone. And as we let our own light shine, we unconsciously give other people permission to do the same. As we are liberated from our own fear, our presence automatically liberates others.",
     source: 'Timo Cruz',
     citation: 'Coach Carter',
-    year: '2005'
+    year: '2005',
+    tag: 'Inspirational'
   }
 ];
 console.log(quotes);
@@ -53,7 +59,9 @@ function getRandomQuote () {
   return randomNumber;
 }
  
-console.log (getRandomQuote);
+console.log (getRandomQuote());
+
+
 
 
 /***
@@ -72,12 +80,22 @@ function printQuote () {
         quoteDisplay += '<span class = "year">' + myQuote.year + '</span>';
        };
 
+    if (myQuote.tag) {   
+       quoteDisplay += '<span class = "year">' + myQuote.year + '</span>';
+       };
+      
        quoteDisplay += '</p>'
+
        
        document.getElementById('quote-box').innerHTML = quoteDisplay; 
-} 
 
-printQuote(); 
+      } 
+
+console.log(printQuote());
+
+printQuote();
+
+ 
 
 
 /***
